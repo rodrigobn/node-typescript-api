04f47057df5cdf7cd98fbacfa236edc0471789e4
Rodrigo Brito do Nascimento <rodrito90@gmail.com>
2021-04-01 19:52:59 -0300

    Node.js API [C02P01-3] - Como utilizar Jest com Typescript da maneira certa com Jest Mocks

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -5,14 +5,15 @@ import stormGlassNormalized3hoursFixture from '@test/fixtures/stormGlass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
-    it('should return the normalized forecast from from the Stormglass service', async() => {
-        const lat = -33.792726;
-        const lng = 151.289824;
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+    it('should return the normalized forecast from from the Stormglass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue({data: stormGlassWeather3hoursFixture});
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3hoursFixture });
 
-        const stormGlass = new StormGlass(axios);
-        const reponse = await stormGlass.fetchPoints(lat, lng);
-        expect(reponse).toEqual(stormGlassNormalized3hoursFixture)
-    });
+    const stormGlass = new StormGlass(mockedAxios);
+    const reponse = await stormGlass.fetchPoints(lat, lng);
+    expect(reponse).toEqual(stormGlassNormalized3hoursFixture);
+  });
 });
